b11c85bebde3457a7eb3091b32e5506a3e8afb5c
Samuel Felipe Castro Fernandes <78573679+samuelfcf@users.noreply.github.com>
2021-10-24 13:48:22 -0300

    update schema

--- src/schemas/TransactionSchema.js
+++ src/schemas/TransactionSchema.js
@@ -3,7 +3,7 @@ import joi from "joi";
 const TransactionSchema = joi.object({
   user_id: joi.number().integer().min(1).required(),
   description: joi.string().empty().required(),
-  value: joi.number().strinct().required(),
+  value: joi.number().strict().required(),
   inflow: joi.boolean().required(),
   date: joi.date().iso().required(),
   id: joi.any().forbidden()
