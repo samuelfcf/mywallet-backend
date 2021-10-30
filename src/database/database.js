042e8cc2a05062ed2deca5fc848f5f70e544f7ab
Samuel Felipe <samuelfelipef@gmail.com>
2021-10-30 16:01:35 -0300

    prepare for deploy

--- src/database/database.js
+++ src/database/database.js
@@ -1,13 +1,14 @@
-import pg from "pg";
+import pg from 'pg';
 
-const { Pool } = pg
+const { Pool } = pg;
 
-const connection = new Pool({
-  user: 'postgres',
-  password: 'semsenha',
-  host: 'localhost',
-  port: 5432,
-  database: 'my_wallet'
-});
+const databaseConfig = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+}
+
+const connection = new Pool(databaseConfig);
 
 export default connection;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import app from "./app.js";
 
-app.listen(4000, () => {
+app.listen(process.env.PORT, () => {
   console.clear();
-  console.log("Server is running on port 4000");
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
