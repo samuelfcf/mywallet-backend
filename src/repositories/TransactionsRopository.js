4902015b9337da8f21a3903269744c41407d4708
Samuel Felipe <samuelfelipef@gmail.com>
2021-11-24 16:17:32 -0300

    create transactions repository

---
+++ src/repositories/TransactionsRopository.js
@@ -0,0 +1,21 @@
+import connection from '../database/connection.js';
+
+class TransactionsRepository {
+  async createNewTransaction({ userId, value, description, inflow, date }) {
+    await connection.query(
+      `INSERT INTO transactions (user_id, value, description, inflow, date) VALUES ($1, $2, $3, $4, $5)`,
+      [userId, value, description, inflow, date]
+    );
+  }
+
+  async getUserTransactions({ userId }) {
+    const result = await connection.query(
+      `SELECT * FROM transactions WHERE user_id = $1`,
+      [userId]
+    );
+    const transactions = result.rows[0];
+    return transactions;
+  }
+}
+
+export default TransactionsRepository;
